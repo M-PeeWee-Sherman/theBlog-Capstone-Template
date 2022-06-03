6a2b0db00165ead9e270dd368a08879a3eaf8bea
PeeWee <AlphaSherman@gmail.com>
2022-06-03 19:35:00 -0400

    moves compiledList within Post list to be in teh correct if/then so taht Post isn't craeted from sentinel -1 value

--- ui/src/Components/PostList.js
+++ ui/src/Components/PostList.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import Post from './Post'
 import Stack from '@mui/material/Stack';
-
+import Typography from '@mui/material/Typography';
 import {SpinningCircles} from 'react-loading-icons'
 
 const PostList=({updateFn,deleteFn,entryList})=> {
-    let compiledPosts = entryList.map((element)=>(
-        <Post key={`Post_${element.id}`} updateFn={updateFn} deleteFn={deleteFn} entry={element}/>
-    ))
+
     if (entryList.length===0) {
+        return(<Typography>No Entries Available</Typography>)
+    } else if(entryList[0]===-1){
         return(<SpinningCircles
-              fill="#06bcee"
-              fillOpacity={1}
-              height="3em"
-              speed={1}
-              stroke="#06bcee"
-              strokeOpacity={1}
-              strokeWidth={2}
-            />)
-    } else {
+            fill="#06bcee"
+            fillOpacity={1}
+            height="3em"
+            speed={1}
+            stroke="#06bcee"
+            strokeOpacity={1}
+            strokeWidth={2}
+          />)
+    }
+    else {
+        let compiledPosts = entryList.map((element)=>(
+            <Post key={`Post_${element.id}`} updateFn={updateFn} deleteFn={deleteFn} entry={element}/>
+        ))
         return ([<Stack 
                 key="stack_1"
                 direction="column" 
