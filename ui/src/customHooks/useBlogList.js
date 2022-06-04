bc9b310eeccf3990fbf54c8fefed8ecb61770e68
PeeWee <AlphaSherman@gmail.com>
2022-06-03 22:27:31 -0400

    reqrite useBlog to be redundant but always pull userNames directly

--- ui/src/customHooks/useBlogList.js
+++ ui/src/customHooks/useBlogList.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from 'react';
-import useUsersList from './useUsersList';
+import  { useState, useEffect } from 'react';
 
 import config from '../config'
 const baseURL = config[process.env.REACT_APP_NODE_ENV || "development"].apiUrl;
@@ -8,38 +7,39 @@ const baseURL = config[process.env.REACT_APP_NODE_ENV || "development"].apiUrl;
 
 const useBloglist = () => {
     const [postList, setPostList] = useState([]);
-    const [nameList, setNameList, updateUsers] = useUsersList();
+    //const [nameList, setNameList, updateUsers] = useUsersList();
     const [combinedList, setCombinedList] = useState([]);
     const [update, setUpdate] = useState(0);
     const updateBlogFn = ()=>{setUpdate(update+1);};
-
-    console.log(React,setNameList,updateUsers);    
+    const [nameList, setNameList] = useState([]);
     //pull total blog list    
     useEffect(()=>{
+        let urlNames = `${baseURL}/users`;
+        const fetchNames = fetch(urlNames);
         let urlPosts = `${baseURL}/posts`;
-        fetch(urlPosts)
-        .then((res) => res.json())
-        .then((data) => {
-  
-            setPostList(data);
-        },(err) => console.log(`useBlogList: ${err}`));
+        const fetchPosts = fetch(urlPosts);
+
+        Promise.all(fetchNames, fetchPosts)
+        .then(([namesRaw, postsRaw]) => [namesRaw.json(),postsRaw.json() ])
+        .then(([namesJSON, postsJSON])=>{
+               
+            setNameList(namesJSON);
+            setPostList(postsJSON);
+            console.log(nameList,postList);
+
+            let indexList = namesJSON.map((entry)=>entry.id); 
+            setCombinedList(postsJSON.map((entry)=>{
+            let result = {...entry,user_info:{username:"ERROR"}}
+            let index = indexList.findIndex(entry.users_id);
+            if(index>=0){
+                result.user_info = {...namesJSON[index]};
+            }
+            return {...result};
+        }))
+        })
+        .catch((e)=>{console.log(`useBLogList Error ${e}`)})
+
     },[update])  
-
-    
-    useEffect(() => {     
-        let indexList = nameList.map((entry)=>entry.id);    
-        
-        setCombinedList(postList.map((entry)=>{
-                
-                let result = {...entry,user_info:{username:"ERROR"}}
-                let index = indexList.findIndex(entry.users_id);
-                if(index>=0){
-                    result.user_info = {...nameList[index]};
-                }
-                return result;
-            }))
-    }, [postList, nameList]);
-
 
     return [combinedList, setCombinedList, updateBlogFn];
 }
